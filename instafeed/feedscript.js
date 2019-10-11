6014da12746d3719326481defb9d079822377121
Sven <henderickx.sven@gmail.com>
2019-10-11 12:16:04 +0200

    working on getting facebook posts

--- instafeed/feedscript.js
+++ instafeed/feedscript.js
@@ -6,22 +6,43 @@ $(document).ready(function(){
         userId: 784297742,
         resolution: 'standard_resolution',
         accessToken: '784297742.1677ed0.155b78eba16f4362a7e7599c297cdb8b',
-        template: '<div class="instapic_wrapper"><img class="instapic" src="{{image}}" /><p>{{caption}}</p></div>',
         mock: true,
         success: function(data){
             instaPosts = data;
-            doSomething();
+            showFeed();
         }
 
     });
 
+    FB.api(
+      '/me',
+      'GET',
+      {"fields":"posts{source,link,name,picture,full_picture,created_time,description}"},
+      function(response) {
+          console.log(response);
+      }
+    );
+
     userFeed.run();
 
-    function doSomething(){
+    function showFeed(){
         console.log(instaPosts);
         $.each(instaPosts.data, function(k, v) {
             console.log(v);
-            $('#instafeed').append('<img src="' + v.images.standard_resolution.url + '" />');
+
+            var caption = '';
+            var imagesrc = '';
+            if(v.caption !== null){
+                caption = "<p>" + v.caption.text + "</p>";
+            }
+
+            if(v.images !== null){
+                imagesrc = '<img class="instapic" src="' + v.images.standard_resolution.url + '"/>';
+            }
+
+
+            $('#instafeed').append('<div class="instapic_wrapper">' + imagesrc + ' ' + caption + '</div>');
+
         });
     }
 
