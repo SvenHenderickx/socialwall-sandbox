405bd9fdbef33e300f63c8c093c4353beab89d26
Jan de Sterke <jandesterke@hotmail.com>
2019-12-13 11:53:01 +0100

    Updated the transitions for several elements in the layout.

--- grid/grid.js
+++ grid/grid.js
@@ -2,58 +2,94 @@ window.onload = function () {
 
     let tweet1 = document.getElementById('tweets');
 
-    tweet1.style.opacity = "1";
-    tweet1.style.marginLeft = "0";
+    function tweetsin() {
+        tweet1.style.opacity = "1";
+        tweet1.style.marginLeft = "0";
+    }
+
+    function tweetsout() {
+        tweet1.style.opacity = "0";
+        tweet1.style.marginLeft = "-25%";
+    }
 
     let youtube = document.getElementById('channel-youtube');
     let twitter = document.getElementById('channel-twitter');
     let instagram = document.getElementById('channel-instagram');
     let facebook = document.getElementById('channel-facebook');
 
-    youtube.style.marginTop = "0";
-    youtube.style.opacity = "1";
+    channelsin();
+    tweetsin();
+    setInterval(channelsin, 10000);
+    setInterval(tweetsin, 10000);
 
-    twitter.style.marginTop = "0";
-    twitter.style.opacity = "1";
+    function channelsin() {
+        youtube.style.marginTop = "0";
+        youtube.style.opacity = "1";
 
-    instagram.style.marginTop = "0";
-    instagram.style.opacity = "1";
+        twitter.style.marginTop = "0";
+        twitter.style.opacity = "1";
 
-    facebook.style.marginTop = "0";
-    facebook.style.opacity = "1";
+        instagram.style.marginTop = "0";
+        instagram.style.opacity = "1";
 
-    document.getElementById('channel-youtube').addEventListener('click', scroll);
-
-    function scroll() {
-        $('html, body').animate({
-            scrollTop: $("#full1").offset().top});
+        facebook.style.marginTop = "0";
+        facebook.style.opacity = "1";
     }
 
-    document.getElementById('huts').addEventListener('click', switchtweets);
+    function channelsout() {
+        youtube.style.marginTop = "-5%";
+        youtube.style.opacity = "0";
 
-    function switchtweets() {
-        let tweets1 = document.getElementById('wall2tweets');
+        twitter.style.marginTop = "-5%";
+        twitter.style.opacity = "0";
 
-        tweets1.style.opacity = "0";
-        tweets1.style.display = "none";
-        tweets1.style.marginRight = "-100%";
+        instagram.style.marginTop = "-5%";
+        instagram.style.opacity = "0";
 
-        let tweets2 = document.getElementById('wall2tweets2');
+        facebook.style.marginTop = "-5%";
+        facebook.style.opacity = "0";
+    }
 
-            if (tweets2.classList.contains('hidden')) {
-                // show
-                tweets2.classList.add('tweets-trans');
-                tweets2.clientWidth; // force layout to ensure the now display: block and opacity: 0 values are taken into account when the CSS transition starts.
-                tweets2.classList.remove('hidden');
-                tweets2.style.marginLeft = "0";
-                tweets2.style.opacity = "1";
-            } else {
-                // hide
-                tweets2.classList.add('tweets-trans');
-                tweets2.classList.add('hidden');
-            }
+    function tweets2in() {
+        let tweetswall2 = document.getElementById('wall2tweets');
 
+        tweetswall2.style.marginTop = "0";
+        tweetswall2.style.opacity = "1";
 
+        let instawall2 = document.getElementById('instawall2');
+
+        instawall2.style.marginLeft = "0";
+        instawall2.style.opacity = "1";
+    }
+
+    function tweets2out() {
+        let tweetswall2 = document.getElementById('wall2tweets');
+
+        tweetswall2.style.marginTop = "-5%";
+        tweetswall2.style.opacity = "0";
+
+        let instawall2 = document.getElementById('instawall2');
+
+        instawall2.style.marginLeft = "-5%";
+        instawall2.style.opacity = "0";
+    }
+
+    setTimeout(scrolldown, 5000);
+
+    function scrolldown() {
+        setTimeout(channelsout, 500);
+        setTimeout(tweetsout, 500);
+        setTimeout(tweets2in, 500);
+        $('html, body').animate({
+            scrollTop: $("#full1").offset().top});
+        setTimeout(scrollup, 5000);
+    }
+
+    function scrollup() {
+        setTimeout(tweets2out, 500);
+        $('html, body').animate({
+            scrollTop: $("#full0").offset().top});
+        setTimeout(scrolldown, 5000);
     }
 
 };
