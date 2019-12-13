8eecbc38b73eb6d87631fcbc27c2c70c9ef6c046
Sven Henderickx <henderickx.sven@gmail.com>
2019-12-13 11:53:22 +0100

    working on facedetection

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script type="text/javascript" src="instafeed/instafeed.min.js"></script>
         <script type="text/javascript" src="instafeed/feedscript.js"></script>
+        <script type="text/javascript" src="instafeed/facedetection.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,400i,500,600,700&display=swap" rel="stylesheet" />
         <html>
         <link href="style/style.css" rel="stylesheet" />
@@ -25,5 +26,10 @@
 
             </div>
         </div>
+        <video  id="_webcam" style="display: none;" playsinline></video>
+        <canvas id="_imageData"></canvas>
+
+
+        <script src="instafeed/js/brfv5/brfv5_js_tk141119_v5.1.0_trial_no_modules.js"></script>
     </body>
 </html>
